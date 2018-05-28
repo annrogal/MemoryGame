1e736cb26389e3140b198e67b1d999c288850d22
Anna Rogal <annrogal1@gmail.com>
2018-05-28 20:38:09 +0200

    add timer

---
+++ js/aplication.js
@@ -0,0 +1,26 @@
+const timer = document.getElementById('timer');
+let seconds = 0;
+let minutes = 0;
+
+
+window.addEventListener("load", function(){
+    setGameTimer();
+});
+
+
+function setGameTimer(){
+    console.log('start');
+    timer.innerHTML = "00:00";
+    setInterval(() => {
+        seconds++;
+        seconds < 10 ? seconds = "0" + seconds : seconds;
+
+        if(seconds === 60){
+            minutes++;
+            minutes < 10 ? minutes = "0" + minutes : minutes;
+            seconds = 0;
+        } 
+
+        timer.innerHTML = `${minutes}:${seconds}`;
+    }, 1000);
+}
