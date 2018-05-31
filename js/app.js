ba0bea551b9260dc4ec34fb045c78c1e117a1275
Anna Rogal <annrogal1@gmail.com>
2018-05-31 21:43:49 +0200

    add setInterval and clearInterval to js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,41 @@
+const timer = document.getElementById('timer');
+let seconds = 0;
+let minutes = 0;
+let time;
+
+
+window.addEventListener("load", function(){
+    setGameTimer();
+    resetGameTimer();
+});
+
+
+function setGameTimer(){
+    timer.innerHTML = "00:00";
+    time = setInterval(() => {
+        seconds++;
+        seconds < 10 ? seconds = "0" + seconds : seconds;
+
+        if(seconds === 60){
+            minutes++;
+            minutes < 10 ? minutes = "0" + minutes : minutes;
+            seconds = 0;
+        } 
+
+        timer.innerHTML = `${minutes}:${seconds}`;
+    }, 1000);
+}
+
+function resetGameTimer(){
+    document.getElementsByClassName("restart")[0].addEventListener("click", () => {
+        console.log("test");
+        clearInterval(time);
+        seconds = 0;
+        minutes = 0;
+        setGameTimer();
+    })    
+}
+
 /*
  * Create a list that holds all of your cards
  */
